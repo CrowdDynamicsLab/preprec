61fb350ed72fc31ab4a7105d828a4abfa9907fc6
prrathi <53785742+prrathi@users.noreply.github.com>
2024-10-03 08:14:27 -0500

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,19 +1,26 @@
-# scripts to train datasets included in paper, assuming previous steps in README have been followed
-python -u main.py --dataset douban/douban_music --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
-python -u main.py --dataset douban/douban_movie --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
-python -u main.py --dataset epinions/epinions --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
-python -u main.py --dataset amazon/amazon_office --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
-python -u main.py --dataset amazon/amazon_tool --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+# after previous steps in README have been followed, you must first set up negative samples for testing:
+python main.py --dataset douban/douban_music --save_neg
+python main.py --dataset douban/douban_movie --save_neg
+python main.py --dataset epinions/epinions --save_neg
+python main.py --dataset amazon/amazon_office --save_neg
+python main.py --dataset amazon/amazon_tool --save_neg
 
-# corresponding scripts to evaluate trained model
-python -u main.py --dataset douban/douban_music --train_dir test --state_dict_path res/douban/douban_music/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
-python -u main.py --dataset douban/douban_movie --train_dir test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
-python -u main.py --dataset epinions/epinions --train_dir test --state_dict_path res/epinions/epinions/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
-python -u main.py --dataset amazon/amazon_office --train_dir test --state_dict_path res/amazon/amazon_office/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
-python -u main.py --dataset amazon/amazon_tool --train_dir test --state_dict_path res/amazon/amazon_tool/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --inference_only
+# then to train on datasets
+python main.py --dataset douban/douban_music --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+python main.py --dataset douban/douban_movie --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+python main.py --dataset epinions/epinions --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+python main.py --dataset amazon/amazon_office --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+python main.py --dataset amazon/amazon_tool --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+
+# then to evaluate trained models
+python main.py --dataset douban/douban_music --train_dir test --state_dict_path res/douban/douban_music/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
+python main.py --dataset douban/douban_movie --train_dir test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
+python main.py --dataset epinions/epinions --train_dir test --state_dict_path res/epinions/epinions/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
+python main.py --dataset amazon/amazon_office --train_dir test --state_dict_path res/amazon/amazon_office/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
+python main.py --dataset amazon/amazon_tool --train_dir test --state_dict_path res/amazon/amazon_tool/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --inference_only
 
 # example for zero-shot transfer to another dataset
-python -u main.py --dataset douban/douban_music --train_dir movie_zs_test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --transfer --inference_only
+python main.py --dataset douban/douban_music --train_dir movie_zs_test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --transfer --inference_only
 
 # example for finetuning on subset (by # users) of another dataset
-python -u main.py --dataset douban/douban_music --train_dir movie_fs_test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --fs_transfer --fs_num_epochs 5 --fs_prop 0.5
+python main.py --dataset douban/douban_music --train_dir movie_fs_test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --fs_transfer --fs_num_epochs 5 --fs_prop 0.5
