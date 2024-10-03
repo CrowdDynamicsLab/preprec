2b1e68cce7ada824dcbddef8d2f00625e6fadaf2
prrathi <53785742+prrathi@users.noreply.github.com>
2024-10-03 07:42:05 -0500

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,13 +1,19 @@
-# examples for training model from scratch
-python -u main.py --dataset amazon/amazon_office --model newrec --train_dir newrec_base
-python -u main.py --dataset amazon/amazon_office --model sasrec --train_dir sasrec
-python -u main.py --dataset amazon/amazon_office --model bert4rec --mask_prob 0.5 --train_dir bert4rec
+# scripts to train datasets included in paper, assuming previous steps in README have been followed
+python -u main.py --dataset douban/douban_music --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+python -u main.py --dataset douban/douban_movie --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+python -u main.py --dataset epinions/epinions --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+python -u main.py --dataset amazon/amazon_office --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
+python -u main.py --dataset amazon/amazon_tool --train_dir train --time_embed --monthpop wtembed --weekpop week_embed2
 
-# example for evaluating trained model, corresponds to first model trained above
-python -u main.py --dataset amazon/amazon_office --model newrec --state_dict_path res/amazon/amazon_office/newrec_base/newrec.epoch=201.lr=0.001.layer=2.head=1.hidden=50.maxlen=200.pth --inference_only --mode test --topk 10 5 1
+# corresponding scripts to evaluate trained model
+python -u main.py --dataset douban/douban_music --train_dir test --state_dict_path res/douban/douban_music/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
+python -u main.py --dataset douban/douban_movie --train_dir test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
+python -u main.py --dataset epinions/epinions --train_dir test --state_dict_path res/epinions/epinions/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
+python -u main.py --dataset amazon/amazon_office --train_dir test --state_dict_path res/amazon/amazon_office/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --inference_only
+python -u main.py --dataset amazon/amazon_tool --train_dir test --state_dict_path res/amazon/amazon_tool/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --inference_only
 
-# example for evaluating trained model by groups, corresponds to above
-python -u main.py --dataset amazon/amazon_office --model newrec --state_dict_path res/amazon/amazon_office/newrec_base/newrec.epoch=201.lr=0.001.layer=2.head=1.hidden=50.maxlen=200.pth --inference_only --mode test --topk 10 --eval_quality --quality_size 25
+# example for zero-shot transfer to another dataset
+python -u main.py --dataset douban/douban_music --train_dir movie_zs_test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --transfer --inference_only
 
-# example for transferring to another dataset, corresponds to above
-python -u main.py --dataset amazon/amazon_tool --transfer --model newrec --state_dict_path res/amazon/amazon_office/newrec_base/newrec.epoch=201.lr=0.001.layer=2.head=1.hidden=50.maxlen=200.pth --inference_only --mode test --topk 10 5 1
+# example for finetuning on subset (by # users) of another dataset
+python -u main.py --dataset douban/douban_music --train_dir movie_fs_test --state_dict_path res/douban/douban_movie/train/best.pth --time_embed --monthpop wtembed --weekpop week_embed2 --use_week_eval --week_eval_pop week_wt_embed_adj --fs_transfer --fs_num_epochs 5 --fs_prop 0.5
