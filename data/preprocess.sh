19cce56bbc80657807a2e4adac771c68dd998da2
prrathi <53785742+prrathi@users.noreply.github.com>
2024-10-03 00:11:13 -0500

    Update preprocess.sh

--- data/preprocess.sh
+++ data/preprocess.sh
@@ -1,2 +1,5 @@
-python data.py --dataset ./amazon/amazon_office
-python data.py --dataset ./amazon/amazon_tool
+python data.py --dataset ./douban/douban_music --t1_cutoff 10 --t2_cutoff 2 --week_adj
+python data.py --dataset ./douban/douban_movie --t1_cutoff 10 --t2_cutoff 2 --week_adj
+python data.py --dataset ./epinions/epinions --t1_cutoff 10 --t2_cutoff 2 --week_adj
+python data.py --dataset ./amazon/amazon_office --t1_cutoff 10 --t2_cutoff 2 --week_adj
+python data.py --dataset ./amazon/amazon_tool --t1_cutoff 10 --t2_cutoff 2 --week_adj
